53715a9d38d359ecb4a435490db38ffc974f5878
Randall Leeds <randall.leeds@gmail.com>
2010-09-03 01:20:46 -0700

    ets-based ref counter
    should be faster but needs more eyes
    this is race condition prone and I want to be sure it's right

--- src/couchdb/couch_ref_counter.erl
+++ src/couchdb/couch_ref_counter.erl
@@ -13,79 +13,109 @@
 -module(couch_ref_counter).
 -behaviour(gen_server).
 
+-include("couch_db.hrl").
+
 -export([start/1, init/1, terminate/2, handle_call/3, handle_cast/2, code_change/3, handle_info/2]).
 -export([drop/1,drop/2,add/1,add/2,count/1]).
 
 start(ChildProcs) ->
     gen_server:start(couch_ref_counter, {self(), ChildProcs}, []).
 
-
 drop(RefCounterPid) ->
     drop(RefCounterPid, self()).
 
 drop(RefCounterPid, Pid) ->
-    gen_server:call(RefCounterPid, {drop, Pid}).
-
+    case
+    catch ets:update_counter(couch_ref_counter, {RefCounterPid, Pid}, -1) of
+    0 ->
+        ets:delete_object(couch_ref_counter, {{RefCounterPid, Pid}, 0}),
+        ets:update_counter(couch_ref_counter, {total, RefCounterPid}, -1),
+        gen_server:cast(RefCounterPid, {drop, Pid});
+    _ ->
+        ok
+    end.
 
 add(RefCounterPid) ->
     add(RefCounterPid, self()).
 
 add(RefCounterPid, Pid) ->
-    gen_server:call(RefCounterPid, {add, Pid}).
+    case
+    catch ets:update_counter(couch_ref_counter, {RefCounterPid, Pid}, 1) of
+    1 ->
+        gen_server:cast(RefCounterPid, {add, Pid});
+    {'EXIT', _} ->
+        ets:update_counter(couch_ref_counter, {total, RefCounterPid}, 1),
+        ets:insert_new(couch_ref_counter, {{RefCounterPid, Pid}, 0}),
+        add(RefCounterPid, Pid);
+    _ ->
+        ok
+    end.
 
 count(RefCounterPid) ->
-    gen_server:call(RefCounterPid, count).
+    ets:lookup_element(couch_ref_counter, {total, RefCounterPid}, 2).
+    %% ets:foldl(fun({total, _}, Acc) ->
+    %%                   Acc;
+    %%              ({{monitor, _}, _}, Acc) ->
+    %%                   Acc;
+    %%              (_, Acc) ->
+    %%                   Acc + 1
+    %%           end, 0, RefCounterTab).
 
 % server functions
 
 -record(srv,
     {
-    referrers=dict:new(), % a dict of each ref counting proc.
     child_procs=[]
     }).
 
 init({Pid, ChildProcs}) ->
     [link(ChildProc) || ChildProc <- ChildProcs],
-    Referrers = dict:from_list([{Pid, {erlang:monitor(process, Pid), 1}}]),
-    {ok, #srv{referrers=Referrers, child_procs=ChildProcs}}.
+    maybe_create_ets_table(),
+    ets:insert(couch_ref_counter, {{total, self()}, 1}),
+    ets:insert(couch_ref_counter, {{self(), Pid}, 1}),
+    ets:insert(couch_ref_counter, {{monitor, self(), Pid},
+                                   erlang:monitor(process, Pid)}), 
+    {ok, #srv{child_procs=ChildProcs}}.
 
 
 terminate(_Reason, #srv{child_procs=ChildProcs}) ->
     [couch_util:shutdown_sync(Pid) || Pid <- ChildProcs],
     ok.
 
+handle_call(Msg, _From, Srv) ->
+    exit({unknown_msg,Msg}).
 
-handle_call({add, Pid},_From, #srv{referrers=Referrers}=Srv) ->
-    Referrers2 =
-    case dict:find(Pid, Referrers) of
-    error ->
-        dict:store(Pid, {erlang:monitor(process, Pid), 1}, Referrers);
-    {ok, {MonRef, RefCnt}} ->
-        dict:store(Pid, {MonRef, RefCnt + 1}, Referrers)
+handle_cast({add, Pid}, Srv) ->
+    case ets:lookup(couch_ref_counter, {self(), Pid}) of
+    [0] ->
+        ok;
+    _ ->
+        case ets:lookup(couch_ref_counter, {monitor, self(), Pid}) of
+        [MonRef] ->
+            ok;
+        _ ->
+            ets:insert_new(couch_ref_counter, {{monitor, self(), Pid},
+                                               erlang:monitor(process, Pid)})
+        end
     end,
-    {reply, ok, Srv#srv{referrers=Referrers2}};
-handle_call(count, _From, Srv) ->
-    {monitors, Monitors} =  process_info(self(), monitors),
-    {reply, length(Monitors), Srv};
-handle_call({drop, Pid}, _From, #srv{referrers=Referrers}=Srv) ->
-    Referrers2 =
-    case dict:find(Pid, Referrers) of
-    {ok, {MonRef, 1}} ->
+    {noreply, Srv};
+handle_cast({drop, Pid}, Srv) ->
+    case ets:lookup(couch_ref_counter, {self(), Pid}) of
+    [0] ->
+        MonRef =
+            ets:lookup_element(couch_ref_counter, {monitor, self(), Pid}, 2),
         erlang:demonitor(MonRef, [flush]),
-        dict:erase(Pid, Referrers);
-    {ok, {MonRef, Num}} ->
-        dict:store(Pid, {MonRef, Num-1}, Referrers);
-    error ->
-        Referrers
+        ets:delete(couch_ref_counter, {monitor, self(), Pid});
+    _ ->
+        ok
     end,
-    Srv2 = Srv#srv{referrers=Referrers2},
+
     case should_close() of
     true ->
-        {stop,normal,ok,Srv2};
+        {stop, normal, Srv};
     false ->
-        {reply, ok, Srv2}
-    end.
-
+        {noreply, Srv}
+    end;
 handle_cast(Msg, _Srv)->
     exit({unknown_msg,Msg}).
 
@@ -93,19 +123,32 @@ handle_cast(Msg, _Srv)->
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
-handle_info({'DOWN', MonRef, _, Pid, _}, #srv{referrers=Referrers}=Srv) ->
-    {ok, {MonRef, _RefCount}} = dict:find(Pid, Referrers),
-    Srv2 = Srv#srv{referrers=dict:erase(Pid, Referrers)},
+handle_info({'DOWN', MonRef, _, Pid, _}, Srv) ->
+    ets:delete_object(couch_ref_counter, {{monitor, self(), Pid}, MonRef}),
+    ets:update_counter(couch_ref_counter, {total, self()}, -1),
+    ets:delete(couch_ref_counter, {self(), Pid}),
+
     case should_close() of
     true ->
-        {stop,normal,Srv2};
+        {stop, normal, Srv};
     false ->
-        {noreply,Srv2}
+        {noreply, Srv}
     end.
 
+maybe_create_ets_table() ->
+    case catch ets:new(couch_ref_counter,
+                       [public, named_table, {write_concurrency, true}]) of
+    couch_ref_counter ->
+        catch ets:give_away(couch_ref_counter, whereis(couch_server), ok);
+    _ -> ok
+    end.
 
 should_close() ->
-    case process_info(self(), monitors) of
-    {monitors, []} ->   true;
-    _ ->                false
+    % Use ets:delete_object/2 to erase the total count if it is exactly
+    % zero. This will cause add/2 and add/3 to fail with badarg.
+    % At this point, we can shut down the server safely.
+    ets:delete_object(couch_ref_counter, {{total, self()}, 0}),
+    case ets:lookup(couch_ref_counter, {total, self()}) of
+    [] -> true;
+    _ -> false
     end.
